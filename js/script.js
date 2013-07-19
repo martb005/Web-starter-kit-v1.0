4091b7a6d3af8a2e494195a3b0897fc298c763d7
Martin <Bussieres>
2013-07-19 10:58:48 -0400

    Fixing SVG Fallback

--- js/script.js
+++ js/script.js
@@ -13,3 +13,12 @@ $(document).ready(function() {
 	}
 
 });
+
+$(document).ready(function() {
+	// SVG to PNG
+	if ($.browser.msie && $.browser.version < 9) {
+		$('img[src$=".svg"]').each(function() {
+			$(this).attr('src', $(this).attr('src').replace('.svg', '.png'));
+		});
+	}
+});
