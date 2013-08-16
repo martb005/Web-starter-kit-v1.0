c3c38dab728cb4ef82292f8a3e2f759bbdf5fa6f
Martin <Bussieres>
2013-08-16 13:17:37 -0400

    Javascript and htaccess
    Put Javascript file at the end of page. Create HTACCESS file to set
    Header Expires et Etags for speed optimisation.

--- footer.php
+++ footer.php
@@ -1,4 +1,7 @@
 	<footer>
 		footer
 	</footer>
+	<!-- js -->
+	<script type="text/javascript" src="js/jquery-1.8.3-min.js"></script>
+	<script type="text/javascript" src="js/script-min.js"></script>
 </html>
