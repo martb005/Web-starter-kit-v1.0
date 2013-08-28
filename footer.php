4649e8089dbe513c24049bbc3e968e6d910d8a44
Martin <Bussieres>
2013-08-28 10:36:36 -0400

    Activate Gzip
    Compress components with gzip in HTACCESS.
    Footer fix for javascript file

--- footer.php
+++ footer.php
@@ -2,7 +2,6 @@
 			footer
 		</footer>
 		<!-- js -->
-		<script type="text/javascript" src="js/jquery-1.8.3-min.js"></script>
 		<script type="text/javascript" src="js/script-min.js"></script>
 	</body>
 </html>
