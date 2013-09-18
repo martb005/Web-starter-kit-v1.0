4bccf3e8dc12fdd6eba3ea7bde52f7752e9c954c
Martin <Bussieres>
2013-09-18 16:02:08 -0400

    Inplut Placeholder fallback
    Add fallback to transform input placeholder to value for browsers who
    does not support placeholder.

--- js/script.js
+++ js/script.js
@@ -16,9 +16,21 @@ jQuery(function($) {
 
 });
 
-// Contact form
 $(function() {
+	// Contact form
 	$('#form-contact').submit(function() { // Replace usual .PHP form action by javascript to decrease SPAM
 		$(this).attr('action','sendmail.php');
 	});
+
+	// Transform placeholder to value for old browsers.
+	if (!Modernizr.input.placeholder){
+		$('input[placeholder],textarea[placeholder]').each(function() {
+			$(this).data('placeholder',$(this).attr('placeholder'));
+			$(this).focus(function() {
+				if ($(this).val()==$(this).data('placeholder'))  $(this).val('');
+			}).blur(function() { 
+				if ($(this).val()=='')  $(this).val($(this).data('placeholder'));
+			}).blur();
+		});
+	};
 });
