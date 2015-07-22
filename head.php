7473c36b3fd21208df776c403418939534a8c0d9
martb <martinbussieres@iMac-de-Martin.local>
2015-07-22 10:17:01 -0400

    CDN
    Remplacement des fichiers JS shiv et respond pour des CDN

--- head.php
+++ head.php
@@ -16,8 +16,8 @@
 		<!-- CSS -->
 		<link type="text/css" rel="stylesheet" href="css/style.css" media="all">
 		<!--[if lt IE 9]>
-			<script src="js/html5shiv.min.js"></script>
-			<script src="js/respond.min.js"></script>
+			<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+			<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
 			<link type="text/css" rel="stylesheet" href="css/old-ie.css" media="all">
 		<![endif]-->
 	</head>
